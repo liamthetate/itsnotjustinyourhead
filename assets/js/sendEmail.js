cbaaa4106e7d2a9fe84e0927939dc0567c809260
Liam Tate <mrtea@protonmail.com>
2021-08-19 14:27:34 +0100

    email

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -8,6 +8,7 @@ function sendMail(contactForm) {
     .then(
         function(response) {
             console.log('worked!', response);
+            document.getElementById('submit_btn').value = "Message sent!"
             document.getElementById('contact-form').reset();
             document.getElementById('onSubmitText').innerHTML = "<p>Message sent!</p>";
         },
