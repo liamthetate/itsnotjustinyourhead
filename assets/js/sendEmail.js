4cfd99859fbc333b31c948733dc29292e14ceb5b
Liam Tate <mrtea@protonmail.com>
2021-08-19 11:09:13 +0100

    added emailjs function

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,16 @@
+function sendMail(contactForm) {
+    emailjs.send("service_injiyh", "template_injiyh", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "subject": contactForm.subject.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log('worked!', response);
+        },
+        function(error) {
+            console.log('failed, loser', error);
+        });
+    return false;
+}
